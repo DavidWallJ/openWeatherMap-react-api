dcfbc04572542be54445bd91a488bc98513345cd
DavidWallJ <djkindread@gmail.com>
2017-05-22 20:10:34 +0800

    troubleshooting heroku

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -4,12 +4,13 @@
 
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=e3415ebd8f9d067c5f2af6196d4e5a11=metric';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?APPID=e3415ebd8f9d067c5f2af6196d4e5a11&units=metric';
 
 module.exports = {
   getTemp: function (location) {
     var encodedLocation = encodeURIComponent(location);
     var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+    console.log(requestUrl);
 
     return axios.get(requestUrl).then(function (res) {
       if (res.data.cod && res.data.message) {
