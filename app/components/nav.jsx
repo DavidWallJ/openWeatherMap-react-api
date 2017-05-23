3bc908656d20f92fbf0ae6f9b38a7b21dd0c4614
DavidWallJ <djkindread@gmail.com>
2017-05-23 18:34:43 +0800

    added foundations

--- app/components/nav.jsx
+++ app/components/nav.jsx
@@ -1,18 +1,46 @@
 import React from 'react'
 import { Link, IndexLink } from 'react-router'
 
-const Nav = (props) => {
-  return (
-    <div>
-      <h2>Nav Component</h2>
-      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-      {/*this one is IndexLink instead of Link because of the way weather is nested in the routing.  Without doing this the active class stuff doesn't work right*/}
-      <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-      <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-      {/*this activeClassName property is a function of react-router*/}
-      {/*its an easy way to set the active class to the active link*/}
-    </div>
-  );
-};
+const Nav = React.createClass({
+  onSearch: function (e) {
+    e.preventDefault();
+    alert('Not yet wired up!');
+  },
+  render: function () {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Weather App</li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+              {/*this one is IndexLink instead of Link because of the way weather is nested in the routing.  Without doing this the active class stuff doesn't work right*/}
+            </li>
+            <li>
+              <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+            </li>
+            <li>
+              <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+              {/*this activeClassName property is a function of react-router*/}
+              {/*its an easy way to set the active class to the active link*/}
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="search" placeholder="Search weather"/>
+              </li>
+              <li>
+                <input type="submit" className="button" value="Get Weather"/>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    );
+  }
+})
 
 module.exports = Nav;
